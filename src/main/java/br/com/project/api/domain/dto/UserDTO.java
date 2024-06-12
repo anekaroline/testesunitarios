daa27bf2f05e159707f564cf6246ddda954ab9ff
Ane Karoline da Silva Pereira <anekarolinespereira@gmail.com>
2024-06-12 16:10:27 -0400

    adicionando anotação @NoArgsConstructor

--- src/main/java/br/com/project/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/project/api/domain/dto/UserDTO.java
@@ -3,9 +3,11 @@ package br.com.project.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class UserDTO {
